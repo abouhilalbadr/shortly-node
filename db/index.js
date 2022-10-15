cd2e6d586d01f8bbfcc819498f2dcf47f0d6cb3a
badr abouhilal <badrabouhilal@MacBook-Pro.local>
2022-10-15 16:32:47 +0100

    change DB env

--- db/index.js
+++ db/index.js
@@ -3,11 +3,11 @@ const validUrl = require('valid-url');
 const shortid = require('shortid');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'shortly',
-  password: 'postgres',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 })
 
 
